727d25ec2d3cb14d1b56586060aef0b203f8f22e
kWeglinski <krzysiek@csth.pl>
2015-10-14 13:32:47 +0200

    screenshot ssl fix

--- serv/app/services/screenshot.js
+++ serv/app/services/screenshot.js
@@ -3,7 +3,7 @@ var webshot = require('webshot');
 function Screen(path) {
   console.log("STARTED SCREENGRAB");
   var filename = path.substr(path.indexOf("://")+3, path.length);
-  webshot(path, 'app/screens/' + filename + '.png', function(err) {
+  webshot(path, 'app/screens/' + filename + '.png', {phantomConfig: {'ignore-ssl-errors': 'true'}}, function(err) {
     console.log("SCREEN TAKEN FOR:"+filename);
   });
 }
