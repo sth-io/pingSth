6e1579d017fb1d97dc042274516c0b84a7ad26f1
kWeglinski <krzysiek@csth.pl>
2015-10-18 21:43:00 +0200

    email to all owners

--- serv/app/services/email.js
+++ serv/app/services/email.js
@@ -1,12 +1,12 @@
-
-var nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer'),
+  config = require('../../config');
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'krzysztofkpyta@gmail.com',
-        pass: 'owslen69xD'
-    }
+  service: config.email.type,
+  auth: {
+    user: config.email.user,
+    pass: config.email.password
+  }
 });
 
 
@@ -15,24 +15,29 @@ function email(type, data) {
 }
 
 handler = {
-    'error': function(data) {
-      // setup e-mail data with unicode symbols
+  'error': function(data) {
+    // setup e-mail data with unicode symbols
+    if( Object.prototype.toString.call( data.owner ) !== '[object Array]' ) {
+      data.owner = [ data.owner ];
+    }
+    for (var i = 0, len = data.owner.length; i < len; i++) {
       var mailOptions = {
-          from: '« PingSth <pingsth@gmail.com>', // sender address
-          to: 'krzysztofweglinski@gmail.com', // list of receivers
-          subject: '[PingSth] Website Alert', // Subject line
-          text: 'Your website ' + data.website + ' has broke down', // plaintext body
-          html: '<h1>Your website alarm</h1> <p><a href="' + data.website + '">' + data.website + '</a> is not accesible through ping.</p>' // html body
+        from: '« PingSth <' + config.email.user + '>', // sender address
+        to: data.owner[i], // list of receivers
+        subject: '[PingSth] Website Alert', // Subject line
+        text: 'Your website ' + data.website + ' has broke down', // plaintext body
+        html: '<h1>Your website alarm</h1> <p><a href="' + data.website + '">' + data.website + '</a> is not accesible through ping.</p>' // html body
       };
 
-      transporter.sendMail(mailOptions, function(error, info){
-          if(error){
-              return console.log(error);
-          }
-          console.log('✔ Message sent. Response:'.cyan + info.response);
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('✔ Message sent. Response:'.cyan + info.response);
 
       });
     }
+  }
 }
 
 module.exports = email;
